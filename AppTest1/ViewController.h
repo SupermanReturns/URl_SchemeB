86938268e75e9f546153c936cabedfd243ba4546
Superman <703353105@qq.com>
2018-03-01 16:25:51 +0800

    first commit

---
+++ AppTest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppTest1
+//
+//  Created by Superman on 2018/3/1.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AppTest1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AppTest1
+//
+//  Created by Superman on 2018/3/1.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
